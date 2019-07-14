3f51a380fce99f2eefa6718611b2963d05446935
Rajeev Ranjan <rajeevranjan@Mac-mini.local>
2019-07-14 14:17:07 +0530

    EsLint

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { StorageService ,UserDetail, Logs, Feedback} from '../service/browser-storage.service';
+import { StorageService, UserDetail, Logs, Feedback } from '../service/browser-storage.service';
 import { Router } from '@angular/router';
 import { PopUpProp, PopUpComponent } from '../pop-up/pop-up.component';
 import { FloatTextComponent } from '../float-text/float-text.component';
@@ -18,53 +18,53 @@ export class AdminComponent implements OnInit {
   public allUsers: Array<UserDetail>;
   public userLogs: Array<Logs>;
   public feedbacks: Array<Feedback>;
-  public popUpProp : PopUpProp;
-  public tempUser : UserDetail;
-  public count : number;
-  public isAdmin :boolean;
-  public isGuestUser : boolean;
-  public loggedUserID : number;
-  public currentUserName : string;
+  public popUpProp: PopUpProp;
+  public tempUser: UserDetail;
+  public count: number;
+  public isAdmin: boolean;
+  public isGuestUser: boolean;
+  public loggedUserID: number;
+  public currentUserName: string;
 
-  @ViewChild('newUserConfirmation') public newUserConfirmation : PopUpComponent;
-  @ViewChild('floater') floater : FloatTextComponent;
-  @ViewChild('feedback') feedback : FeedbackComponent;
-  @ViewChild('logModal') logModal : ModalDirective; 
-  @ViewChild('feedbackModal') feedbackModal : ModalDirective; 
+  @ViewChild('newUserConfirmation') public newUserConfirmation: PopUpComponent;
+  @ViewChild('floater') floater: FloatTextComponent;
+  @ViewChild('feedback') feedback: FeedbackComponent;
+  @ViewChild('logModal') logModal: ModalDirective;
+  @ViewChild('feedbackModal') feedbackModal: ModalDirective;
 
-  public testURL='./';
+  public testURL = './';
   public postData = `{"un":"uid","fn":"sample user","pw":"passtext"}`;
   public testResponse = '';
   public isPost = false;
 
-  constructor(private _title: Title, private _localStorage: StorageService, private _router:Router, private _httpHelper : HttpHelperService) {
-    this._localStorage.checkForLogin();   
+  constructor(private _title: Title, private _localStorage: StorageService, private _router: Router, private _httpHelper: HttpHelperService) {
+    this._localStorage.checkForLogin();
   }
-  
+
   ngOnInit() {
-    this.count=1;
+    this.count = 1;
     this.isAdmin = this._localStorage.isAdmin();
     this.isGuestUser = this._localStorage.isGuestUser;
     this.loggedUserID = this._localStorage.loggedUser.userid;
-    if(this.isAdmin){
+    if (this.isAdmin) {
       this.fetchAllUser();
     } else {
       this.linkedInBadge();
     }
-    this._title.setTitle(this.isAdmin? 'Admin Page':'About Page');
-   
+    this._title.setTitle(this.isAdmin ? 'Admin Page' : 'About Page');
+
   }
 
   public linkedInBadge() {
-    let scripts = document.getElementsByTagName("script");
+    const scripts = document.getElementsByTagName('script');
     for (let i = 0; i < scripts.length; ++i) {
-        if (scripts[i].getAttribute('src') && scripts[i].getAttribute('src').includes("platform.linkedin.com")) {
-            scripts[i].remove();
-        }
+      if (scripts[i].getAttribute('src') && scripts[i].getAttribute('src').includes('platform.linkedin.com')) {
+        scripts[i].remove();
+      }
     }
 
-    let linkedInScripts = 'https://platform.linkedin.com/badges/js/profile.js';
-    let node = document.createElement('script');
+    const linkedInScripts = 'https://platform.linkedin.com/badges/js/profile.js';
+    const node = document.createElement('script');
     node.src = linkedInScripts;
     node.type = 'text/javascript';
     node.async = false;
@@ -72,152 +72,154 @@ export class AdminComponent implements OnInit {
     document.getElementsByTagName('head')[0].appendChild(node);
   }
 
-  public fetchAllUser(){
+  public fetchAllUser() {
     this._localStorage.getAllUsers()
-    .subscribe(res => {
+      .subscribe(res => {
         this.allUsers = res;
-      },error => this.floater.showText(error,'E'));
-    }
+      }, error => this.floater.showText(error, 'E'));
+  }
 
   public deleteUser() {
     this._localStorage.deleteUser(this.tempUser.userid, this.tempUser.username).subscribe(
-      res=> {
-        if(res.status){
+      res => {
+        if (res.status) {
           this.fetchAllUser();
         }
-        this.floater.showText(res.status_message, res.status?'S':'E', 2000);
+        this.floater.showText(res.status_message, res.status ? 'S' : 'E', 2000);
       }
-    )    
+    );
   }
 
-  public deleteUserConfirmation(user: UserDetail){
+  public deleteUserConfirmation(user: UserDetail) {
     this.tempUser = user;
-    let confirmUser = new PopUpProp();
-    confirmUser.header = user.userid > 2 ? `Delete User`: 'Clear all Logs';
+    const confirmUser = new PopUpProp();
+    confirmUser.header = user.userid > 2 ? `Delete User` : 'Clear all Logs';
     confirmUser.body = user.userid > 2 ? `${user.fullname} will no longer be able to login. This will also delete their log history. Do you want to delete ?` : `This will all log history of ${user.fullname}. Do you want to clear ?`;
-    confirmUser.btnDanger = user.userid > 2 ?`Delete ${user.fullname}`:`Clear All Logs`;
+    confirmUser.btnDanger = user.userid > 2 ? `Delete ${user.fullname}` : `Clear All Logs`;
     confirmUser.btnNeutral = 'Cancel';
     confirmUser.operation = 'delete';
     this.popUpProp = confirmUser;
     this.newUserConfirmation.show();
   }
 
-  public goTo(url:string){
+  public goTo(url: string) {
     this._router.navigateByUrl(url);
   }
 
-  public isDelete(user: UserDetail):boolean{
-    return (user.access =='user');
+  public isDelete(user: UserDetail): boolean {
+    return (user.access === 'user');
   }
 
-  public successCallBack(obj: any){
-    if(this.popUpProp.operation == 'delete'){
+  public successCallBack(obj: any) {
+    if (this.popUpProp.operation === 'delete') {
       this.deleteUser();
-    } else if(this.popUpProp.operation == 'create'){
-      obj.res=='I' ? this.createDummyUser() : this.goTo('login');
+    } else if (this.popUpProp.operation === 'create') {
+      obj.res === 'I' ? this.createDummyUser() : this.goTo('login');
     }
   }
 
-  public createDummyUser(){
-    this._localStorage.CreateDummyUser(5).subscribe(res=>{
-      if(res){
+  public createDummyUser() {
+    this._localStorage.CreateDummyUser(5).subscribe(res => {
+      if (res) {
         this.floater.showText('Dummy User Created Successfully', 'I', 2000);
         this.fetchAllUser();
-      }else{
+      } else {
         this.floater.showText(`Dummy User creation failed`, 'E', 2000);
       }
-    })
-    
+    });
+
   }
 
-  
-  public createUser(){
-    if(this.allUsers.length > 7){
+
+  public createUser() {
+    if (this.allUsers.length > 7) {
       this.floater.showText('Maximum allowed Users already created', 'E');
       return;
     }
-    let confirmUser = new PopUpProp();
+    const confirmUser = new PopUpProp();
     confirmUser.header = 'Create New User';
     confirmUser.body = `<h5>Are you sure you want to create new User?</h5>
     <h6>You would be taken to Login page, Click on new user on Login page to create a user.
     <h6>Note : The user is created locally. Click on Create Dummy to create a dummy user with same User Name and Password</h6>`;
     confirmUser.btnSuccess = 'SignUp Page';
     confirmUser.btnNeutral = 'Cancel';
-    confirmUser.btnInfo = 'Create Dummy'
-    confirmUser.operation= 'create';
+    confirmUser.btnInfo = 'Create Dummy';
+    confirmUser.operation = 'create';
     this.popUpProp = confirmUser;
     this.newUserConfirmation.show();
   }
 
-  public showTestNotification(){
-    let n = this.count
-    let t = n % 3 == 0 ? 'I' : n % 3 == 1 ?  'E' : 'S';
-    this.floater.showText(this.count++ + '  Test Notification Received', t)
+  public showTestNotification() {
+    const n = this.count;
+    const t = n % 3 === 0 ? 'I' : n % 3 === 1 ? 'E' : 'S';
+    this.floater.showText(this.count++ + '  Test Notification Received', t);
   }
-  
 
-  public testConnection(){
-    if(!this.testURL.startsWith('./') && !this.testURL.startsWith('http')) {this.testURL=='./' + this.testURL;}
+
+  public testConnection() {
+    if (!this.testURL.startsWith('./') && !this.testURL.startsWith('http')) {
+      this.testURL = './' + this.testURL;
+    }
 
     let dataP = '';
-    try{
+    try {
       dataP = JSON.parse(this.postData);
-    } catch(e){
-      this.floater.showText(e,'E');
+    } catch (e) {
+      this.floater.showText(e, 'E');
       return;
     }
 
-    if(this.isPost){
-      this._httpHelper.post(this.testURL, dataP).subscribe(res=>{
+    if (this.isPost) {
+      this._httpHelper.post(this.testURL, dataP).subscribe(res => {
         this.testResponse = res;
-        this.floater.showText("Sucess",'S');
-      },(error:string)=>{
+        this.floater.showText('Sucess', 'S');
+      }, (error: string) => {
         this.testResponse = error;
-        this.floater.showText("Error Occurred",'E');
+        this.floater.showText('Error Occurred', 'E');
       });
     } else {
-      
-      this._httpHelper.get(this.testURL).subscribe(res=>{
+
+      this._httpHelper.get(this.testURL).subscribe(res => {
         this.testResponse = res;
-        this.floater.showText("Sucess",'S');
-      },(error:string)=>{
+        this.floater.showText('Sucess', 'S');
+      }, (error: string) => {
         this.testResponse = error;
-        this.floater.showText("Error Occurred",'E');
+        this.floater.showText('Error Occurred', 'E');
       });
 
     }
 
-    
+
   }
 
-  public openLogModal(userId : number){
-    this.currentUserName = this.allUsers? this.allUsers.find(x=> x.userid == userId).fullname:'you';
+  public openLogModal(userId: number) {
+    this.currentUserName = this.allUsers ? this.allUsers.find(x => x.userid === userId).fullname : 'you';
     this._localStorage.getLogforUser(userId).subscribe(
-      (logs:Array<Logs>)=>{
-        if(logs && logs.length > 0){
-           this.userLogs = logs;
+      (logs: Array<Logs>) => {
+        if (logs && logs.length > 0) {
+          this.userLogs = logs;
           this.logModal.show();
         } else {
-          this.floater.showText(`No logs available for ${this.currentUserName}`,'I');
+          this.floater.showText(`No logs available for ${this.currentUserName}`, 'I');
         }
-      },(error:string)=>{this.floater.showText(error,'E')});
+      }, (error: string) => { this.floater.showText(error, 'E'); });
   }
 
-  public openFeedbackModal(userId : number){
-    this.currentUserName = this.allUsers? this.allUsers.find(x=> x.userid == userId).fullname:'';
+  public openFeedbackModal(userId: number) {
+    this.currentUserName = this.allUsers ? this.allUsers.find(x => x.userid === userId).fullname : '';
     this._localStorage.getFeedback(userId).subscribe(
-      (feedbacks:Array<Feedback>)=>{
-        if(feedbacks && feedbacks.length > 0){
+      (feedbacks: Array<Feedback>) => {
+        if (feedbacks && feedbacks.length > 0) {
           this.feedbacks = feedbacks;
           this.feedbackModal.show();
-       } else {
-         this.floater.showText(`No feedback available from ${this.currentUserName}`,'I');
-       }
-     },(error:string)=>{this.floater.showText(error,'E')});
+        } else {
+          this.floater.showText(`No feedback available from ${this.currentUserName}`, 'I');
+        }
+      }, (error: string) => { this.floater.showText(error, 'E'); });
   }
 
-  public feedbackThanks(isSuccess: boolean){
-      this.floater.showText('Thank you for your Valuable feedback','I');
+  public feedbackThanks(isSuccess: boolean) {
+    this.floater.showText('Thank you for your Valuable feedback', 'I');
   }
 
 }
--- src/app/dots/dots.component.ts
+++ src/app/dots/dots.component.ts
@@ -6,36 +6,38 @@ import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angu
   styleUrls: ['./dots.component.css']
 })
 export class DotsComponent implements OnInit, OnDestroy {
-  @Input() x: number = 0;
-  @Input() y: number = 0;
+  @Input() x = 0;
+  @Input() y = 0;
   @Input() master = 0;
   @Input() isDotGun = false;
   @Output() outY = new EventEmitter<number>();
 
-  @Input() down: boolean = true;
+  @Input() down = true;
   public dotsId: any;
 
   constructor() { }
 
   ngOnInit() {
-    if(!this.isDotGun)
-    this.dotsId = setInterval(() => {
-      if (this.down) {
-        if(this.master != 2){
-          this.y = this.y + 2;
-          if (this.master == 1 && this.y % 50 == 0) {
-            this.x += Math.random() > 0.5 ? 30 : -30;
+    if (!this.isDotGun) {
+      this.dotsId = setInterval(() => {
+        if (this.down) {
+          if (this.master !== 2) {
+            this.y = this.y + 2;
+            if (this.master === 1 && this.y % 50 === 0) {
+              this.x += Math.random() > 0.5 ? 30 : -30;
+            }
           }
+        } else {
+          this.y = this.y - 20;
         }
-      } else {
-        this.y = this.y - 20;
-      }
-      this.outY.emit(this.y);
-    }, 10);
+        this.outY.emit(this.y);
+      }, 10);
+    }
   }
 
   ngOnDestroy() {
-    if(!this.isDotGun)
-    clearInterval(this.dotsId); 
+    if (!this.isDotGun) {
+      clearInterval(this.dotsId);
+    }
   }
 }
